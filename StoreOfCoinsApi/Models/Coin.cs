bf147b87c403f64f03ee034f1c2f883510d3e049
Artyom <super.a-piliugin@yandex.ru>
2024-10-04 11:16:37 +0300

    firstCommit

---
+++ StoreOfCoinsApi/Models/Coin.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace StoreOfCoinsApi.Models
+{
+    public class Coin
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Country { get; set; } = null!;
+        public decimal Year { get; set; }
+        public string Currency { get; set; } = null!;
+        public decimal Value { get; set; }
+        public decimal Price { get; set; }
+
+      
+    }
+}
